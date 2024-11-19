d04b0bde81f5002af663351b5731bd81128b5493
duru100470 <zenpia@naver.com>
2024-11-20 01:03:53 +0900

    init sqlite

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import express, { Express, Request, Response } from 'express';
+import {openDB} from './database';
+
+const initializeDatabase = async () => {
+  const db = await openDB();
+
+  const initQuery = `
+  CREATE TABLE IF NOT EXISTS users(
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_name VARCHAR(20)
+  )
+  `
+  await db.exec(initQuery);
+  await db.close();
+};
 
 const app: Express = express();
 const port = 5000;
+
+initializeDatabase().catch((err) => {
+  console.error("Error initializing the database:", err);
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Stencilight Rank Server');
@@ -9,6 +27,19 @@ app.get('/', (req: Request, res: Response) => {
 
 app.get('/handshake', (req: Request, res: Response) => {
   res.send('Hello!!');
+});
+
+app.get('/test1', async (req: Request, res: Response) => {
+  const db = await openDB();
+
+  await db.exec(`
+    INSERT INTO users (user_name)
+    VALUES ('duruchigy')
+  `);
+
+  await db.close();
+
+  res.send('GOOD!');
 });
 
 app.listen(port, () => {
